8068716d879a292e4954bd35cfa16a962598171d
bartoszOsn <osinskibartosz24@gmail.com>
2020-09-17 07:48:22 +0200

    Added Panel controller

---
+++ Source/Components/Panel.cpp
@@ -0,0 +1,47 @@
+/*
+  ==============================================================================
+
+    Panel.cpp
+    Created: 17 Sep 2020 7:37:26am
+    Author:  cokol
+
+  ==============================================================================
+*/
+
+#include <JuceHeader.h>
+#include "Panel.h"
+
+//==============================================================================
+Panel::Panel()
+{
+    // In your constructor, you should add any child components, and
+    // initialise any special settings that your component needs.
+
+}
+
+Panel::~Panel()
+{
+}
+
+void Panel::paint (juce::Graphics& g)
+{
+    /* This demo code just fills the component's background and
+       draws some placeholder text to get you started.
+
+       You should replace everything in this method with your own
+       drawing code..
+    */
+
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));   // clear the background
+}
+
+void Panel::resized()
+{
+	int size = sections.size();
+	auto bounds = getLocalBounds();
+	for (int i = 0; i < size; i++)
+	{
+		auto section = sections.getUnchecked(i);
+		section.setBoundsRelative(0.0f, bounds.getHeight() / size * i, 1.0f, bounds.getHeight() / size);
+	}
+}
---
+++ Source/Components/Panel.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+class Panel  : public juce::Component
+{
+public:
+    Panel();
+    ~Panel() override;
+
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+	void addSection(juce::String name);
+	void addSlider(juce::RangedAudioParameter* parameter, int sectionIndex);
+	void addButton(juce::RangedAudioParameter* parameter, int sectionIndex);
+
+private:
+	juce::Array<juce::GroupComponent> setions;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Panel)
+};
