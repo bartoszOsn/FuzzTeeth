6ccf0295104a330570e4a7f2eec77e6ae9e1c7d2
bartoszOsn <osinskibartosz24@gmail.com>
2020-09-18 11:16:12 +0200

    Documented Callback and CallbackManager classes

--- Source/CallbackManager.cpp
+++ Source/CallbackManager.cpp
@@ -1,11 +1,16 @@
 #include "CallbackManager.h"
 
+///<summary>
+///Constructor.
+///See <seealso cref="CallbackManager::create" />
+///</summary>
 Callback::Callback(std::function<void(int, float)> valueChanged, std::function<void(int, bool)> gestureChanged)
 {
 	this->valueChanged = valueChanged;
 	this->gestureChanged = gestureChanged;
 }
 
+///<summary>implementation of <see cref="juce::AudioParameterFloat::Listener::parameterValueChanged" />.</summary>
 void Callback::parameterValueChanged(int parameterIndex, float newValue)
 {
 	if (valueChanged)
@@ -14,6 +19,7 @@ void Callback::parameterValueChanged(int parameterIndex, float newValue)
 	}
 }
 
+///<summary>implementation of <see cref="juce::AudioParameterFloat::Listener::parameterGestureChanged" />.</summary>
 void Callback::parameterGestureChanged(int parameterIndex, bool gestureIsStarting)
 {
 	if (gestureChanged)
@@ -22,6 +28,8 @@ void Callback::parameterGestureChanged(int parameterIndex, bool gestureIsStartin
 	}
 }
 
+///<summary>Destructor</summary>
+///<remarks>Deletes all <see cref="Callback" /> objects created by <see cref="CallbackManager::create" /></remarks>
 CallbackManager::~CallbackManager()
 {
 	for (Callback*& clb : callbacks)
@@ -30,6 +38,10 @@ CallbackManager::~CallbackManager()
 	}
 }
 
+///<summary>Creates object which can be used as argument by <see cref="juce::AudioParameterFloat::addListener" />
+///<remarks>Returned object will be alive as long as <c>this*</c> is.</remarks>
+///<param name="valueChanged">Functor which will be called by <see cref="Callback::parameterValueChanged" />.</param>
+///<param name="gestureChanged">Functor which will be called by <see cref="Callback::parameterGestureChanged" />.</param>
 Callback* CallbackManager::create(std::function<void(int, float)> valueChanged, std::function<void(int, bool)> gestureChanged)
 {
 	Callback* clb = new Callback(valueChanged, gestureChanged);
--- Source/CallbackManager.h
+++ Source/CallbackManager.h
@@ -4,6 +4,8 @@
 
 class Callback;
 
+///<summary>Class which manages instances of <see cref="Callback" />.</summary>
+///<remarks>Instances of <see cref="Callback" /> created by this object will be alive as long, as this object will be.</remarks>
 class CallbackManager
 {
 public:
@@ -11,9 +13,11 @@ public:
 	Callback* create(std::function<void(int, float)> valueChanged, std::function<void(int, bool)> gestureChanged = nullptr);
 
 private:
+	///<summary>Holds all instances of <see cref="Callback" /> created by this object</summary>
 	juce::Array<Callback*> callbacks;
 };
 
+///<summary>It is wrapper around <see cref="juce::AudioParameterFloat::Listener" /> which allows usage of lambdas and std::function.</summary>
 class Callback : juce::AudioParameterFloat::Listener
 {
 public:
@@ -22,6 +26,9 @@ public:
 	friend Callback* CallbackManager::create(std::function<void(int, float)> valueChanged, std::function<void(int, bool)> gestureChanged);
 private:
 	Callback(std::function<void(int, float)> valueChanged, std::function<void(int, bool)> gestureChanged);
+
+	///<summary>Function to be called by <see cref="Callback::parameterValueChanged" />.<summary>
 	std::function<void(int, float)> valueChanged;
+	///<summary>Function to be called by <see cref="Callback::parameterGestureChanged" />.<summary>
 	std::function<void(int, bool)> gestureChanged;
 };
